b0c5e076da871dffbc4b962164a199a37180a6fa
yashpk789987 <yashpk789987@gmail.com>
2021-06-01 02:42:55 +0530

    Added All Scripts

--- generate_token.php
+++ generate_token.php
@@ -6,7 +6,7 @@ require_once "RtcTokenBuilder.php";
 $appID = "b1ff97b3e47244eaa4c0177359705c0f";
 $appCertificate = "b95de5e4627c438db001427d629803b1";
 $channelName = "yashpk212830";
-$uid = 2882341273;
+$uid = 0;
 $role = RtcTokenBuilder::RolePublisher;
 $expireTimeInSeconds = 3600;
 $currentTimestamp = (new DateTime("now", new DateTimeZone('UTC')))->getTimestamp();
