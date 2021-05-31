18759dc3e4dc86db1946caed12267d85ac1c2e16
yashpk789987 <yashpk789987@gmail.com>
2021-06-01 02:48:41 +0530

    Builder Created

--- generate_token.php
+++ generate_token.php
@@ -5,7 +5,7 @@ require_once "RtcTokenBuilder.php";
 
 $appID = "b1ff97b3e47244eaa4c0177359705c0f";
 $appCertificate = "b95de5e4627c438db001427d629803b1";
-$channelName = "yashpk212830";
+$channelName = "+919630884259";
 $uid = 0;
 $role = RtcTokenBuilder::RolePublisher;
 $expireTimeInSeconds = 3600;
@@ -13,7 +13,8 @@ $currentTimestamp = (new DateTime("now", new DateTimeZone('UTC')))->getTimestamp
 $privilegeExpiredTs = $currentTimestamp + $expireTimeInSeconds;
 
 $token = RtcTokenBuilder::buildTokenWithUid($appID, $appCertificate, $channelName, $uid, $role, $privilegeExpiredTs);
-echo 'Token with int uid: ' . $token . PHP_EOL;
+$data = array("token" => $token , "channel" => $channelName);
+echo json_encode($data);
 
 
 
